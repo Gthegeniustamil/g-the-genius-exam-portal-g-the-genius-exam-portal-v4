e24f83bbe5ff03e3cf7837332afb109028921476
Gthegeniustamil <youngvillagefood@gmail.com>
2026-07-24 06:19:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,105 @@ document.addEventListener("DOMContentLoaded", () => {
 // TNUSRB MOCK TEST ENGINE
 // ===============================
 
+// ===============================
+// FIREBASE QUESTION LOADER
+// ===============================
+
+
+import { db } from "./firebase-config.js";
+
+
+import {
+
+collection,
+getDocs
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+
+let questions = [];
+
+let currentQuestion = 0;
+
+let userAnswers = [];
+
+let totalTime = 1200;
+
+let timer;
+
+
+
+const questionBox =
+document.getElementById("question");
+
+
+const optionsBox =
+document.getElementById("options");
+
+
+const palette =
+document.getElementById("palette");
+
+
+
+if(questionBox){
+
+
+loadQuestions();
+
+
+}
+
+
+
+async function loadQuestions(){
+
+
+const snapshot = await getDocs(
+
+collection(db,"questions")
+
+);
+
+
+
+questions=[];
+
+
+snapshot.forEach((doc)=>{
+
+
+questions.push(doc.data());
+
+
+});
+
+
+
+questions = questions
+.sort(()=>Math.random()-0.5)
+.slice(0,20);
+
+
+
+userAnswers =
+new Array(questions.length).fill(null);
+
+
+
+createPalette();
+
+showQuestion();
+
+startTimer();
+
+
+}
 
 // இங்கிருந்து நான் கொடுத்த
 // questions
