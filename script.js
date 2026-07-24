42f0a50c7b4dae12a638f8de954881ce9f624866
Gthegeniustamil <youngvillagefood@gmail.com>
2026-07-24 06:17:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,35 +29,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
-/*
-G THE GENIUS TNUSRB EXAM PORTAL V4
-Main JavaScript File
-*/
-
-
-document.addEventListener("DOMContentLoaded", () => {
-
-
-    console.log("G THE GENIUS TNUSRB Portal Loaded Successfully");
-
-
-    const links = document.querySelectorAll("a");
-
-
-    links.forEach(link => {
-
-        link.addEventListener("click", () => {
-
-            console.log("Opening:", link.href);
-
-        });
-
-    });
-
-
-});
-
-
 
 
 // ===============================
